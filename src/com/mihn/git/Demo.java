3031dc7fe2d8a314c132373c06fc647c71030fab
wangxingming <wangxingming@jd.com>
2017-07-14 15:09:41 +0800

    ces

--- src/com/mihn/git/Demo.java
+++ src/com/mihn/git/Demo.java
@@ -6,6 +6,7 @@ public class Demo {
 		
 		System.out.println("Git learning....");
 		System.out.println("hello jd 1");
-		System.out.println("hello jd 1");
+		System.out.println("hello jd 2");
+		System.out.println("hello jd 3");
 	}
 }
